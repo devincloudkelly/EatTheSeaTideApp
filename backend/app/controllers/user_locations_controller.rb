a460a961840eed4949d04434e08a83a689e0c1ca
devincloudkelly <devin.cloud.kelly@gmail.com>
2019-12-30 15:48:23 -0800

    form logic working, fetchs data based on user input

--- backend/app/controllers/user_locations_controller.rb
+++ backend/app/controllers/user_locations_controller.rb
@@ -1,10 +1,11 @@
 class UserLocationsController < ApplicationController
 
     def index
-        lat = params[:lat]
-        long = params[:long]
+        lat = params[:lat].to_i
+        long = params[:long].to_i
         tides = UserLocation.api(lat, long)
         render json: tides
+        # byebug
     end
 
 end
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,30 +1,47 @@
-document.addEventListener('DOMContentLoaded', (e) =>{
-    console.log(e)
-    const tideForm = document.querySelector('#tide-form')
-    tideForm.addEventListener('submit', (e) => {
-        e.preventDefault()
-        const lat = e.target.latitude.value
-        const long = e.target.longitude.value
-        const locName = e.target['loc-name'].value
-        const tide = e.target['tide-type'].value
-        const tideHeight = e.target['tide-height'].value
-        fetchTides(lat, long)
 
-    })
+//event listener for HTML page load
+document.addEventListener('DOMContentLoaded', (e) =>{
+    
+    //event listener for tide form submission
+    const tideForm = document.querySelector('#tide-form')
+    tideForm.addEventListener('submit', (e) => tideFormSubmission(e))
 })
 
+//tide form event handler
+function tideFormSubmission(e) {
+    e.preventDefault()
+    const lat = e.target.latitude.value
+    const long = e.target.longitude.value
+    const locName = e.target['loc-name'].value
+    const tide = e.target['tide-type'].value
+    const tideHeight = e.target['tide-height'].value
+    fetchTides(lat, long ,tide, tideHeight)
+    
+}
+
 // fetch tides for lat and long passed through form
-const fetchTides = (lat, long) => {
-    fetch(`http://localhost:3000/tides?lat=${lat}&long=${long}`) 
+function fetchTides(lat, long, tide, tideHeight) {
+    return fetch(`http://localhost:3000/tides?lat=${lat}&long=${long}`) 
         .then((resp) => resp.json())
-        .then(data => console.log(data))
+        .then(data => findMatchingTides(data.extremes, tide, tideHeight))
+        // .then(data => findMatchingTides(data["extremes"], tide, tideHeight))
 }
 
 // filter json data to only show tides that match the user's tide height threshold
-const findMatchingTides = (json, tide, tideHeight) => {
-// filter over json, filter out matching tides, then equal or BETTER tide heights (if tide is low, less than, if tide is high, greater than)
-
-//replace temp1. with json
-const userTides = json.extremes.filter(extreme => extreme['state'] === tide)
-if (tide === "LOW TIDE")
+function findMatchingTides(json, tide, tideHeight) {
+    console.log('this is json', json)
+const userTides = json.filter(extreme => extreme.state === tide.split('-').join(' ').toUpperCase())
+// console.log('user tides variable', userTides)
+    if (tide === "low-tide"){
+        console.log('returned low tide')
+        const lowTides = userTides.filter(tide => tide.height <= tideHeight)
+        console.log(lowTides)
+        return lowTides
+    } 
+    if (tide === "high-tide") {
+        console.log('returned high tide')
+        const highTides = userTides.filter(tide => tide.height >= tideHeight)
+        console.log(highTides)
+        return highTides
+    }
 }
